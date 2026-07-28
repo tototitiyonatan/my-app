4ef059ae5cbee3072316587b9ba07b80beff4e83
tototitiyonatan <numreggaeman@gmail.com>
2026-07-28 13:49:42 +0300

    Update exact render backend url

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API_BASE_URL = window.location.hostname === 'localhost'
   ? 'http://127.0.0.1:8000'
-  : 'https://soroka-server.onrender.com'; // <--- הכנס כאן את הכתובת האמיתית מ-Render
+  : 'https://soroka-server.onrender.com';
 
 export default axios.create({
   baseURL: API_BASE_URL,
